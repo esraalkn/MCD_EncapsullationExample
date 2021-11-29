7f260d361eb2b56c4c95cfc00eb2b80234092d7c
Esra Alkan <es.alknn@gmail.com>
2021-11-29 19:48:06 +0300

    Add project files.

---
+++ MCD_EncapsullationExample/Daire.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsullationExample
+{
+    class Daire
+    {
+        #region Ozellikler
+        public double YariCap { get; set; }
+
+        private double _cap = 0;
+
+        public double Cap
+        {
+            get
+            {
+                if (YariCap > 0d)
+                {
+                    _cap = YariCap * 2;
+                }
+
+                return _cap;
+            }
+        }
+
+        public double PiSayisi { get; set; }
+
+        #endregion
+
+        #region Constructorlar Yani Yapıcı Metotlar
+        public Daire()
+        {
+            PiSayisi = 3;
+        }
+
+        public Daire(double DisardanAlinanPiSayisi)
+        {
+            PiSayisi = DisardanAlinanPiSayisi;
+        }
+        #endregion
+
+        #region Metotlar
+
+        public void Alanhesapla()
+        {
+            //pi*r^2
+            double sonuc = PiSayisi * Math.Pow(YariCap, 2);
+            Console.WriteLine("Alan : "+sonuc.ToString("0.00"));
+
+        }
+
+        public void Cevrehesapla()
+        {
+            double sonuc = 2 * PiSayisi * YariCap;
+            Console.WriteLine("Çevre : "+sonuc.ToString("0.00"));
+        }
+        #endregion
+    }
+}
---
+++ MCD_EncapsullationExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsullationExample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            /*
+             * Daire isimli bir class'a aşağıdaki şekilde nesne oluşturma kuralları uygulayınız.
+             * Pi sayısı olarak bir özellik tutunuz. Bu özelliğin değerini 3 atayınız.
+             * Pi sayı değerini dişarıdan gönderiniz.
+             * Bu dairenin çevresini ve alanını hesaplayınız.
+             */
+
+            Daire daire1 = new Daire(4);//dışarıdan girilen pi sayısı
+            daire1.YariCap = 5;
+            Console.WriteLine("Çap : "+daire1.Cap.ToString());
+            daire1.Alanhesapla();
+            Console.WriteLine();
+            daire1.Cevrehesapla();
+            Console.ReadKey();
+
+        }
+    }
+}
